6ff39242404f868e3ca1f8350d676dd17621aa3e
thiercelin-loic <thiercelin.loic1@gmail.com>
2025-09-19 17:04:06 +0200

    refactor: remove unused DTO and entity files for bookings, listings, and reviews

--- src/validations/update-bookings.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookingsDto } from './create-bookings.dto';
-
-export class UpdateBookingsDto extends PartialType(CreateBookingsDto) {}
--- src/validations/update-review.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReviewDto } from 'src/validations/create-review.dto';
-
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
